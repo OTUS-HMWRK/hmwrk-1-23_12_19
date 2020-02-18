b885753963b7e69941827f673a2d3bc8cd6594bd
Kirill Iudov <kirill.iudov@yandex.ru>
2020-02-19 00:01:22 +0300

    first commit!

---
+++ app/app.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+
+#include "version.h"
+
+int main(int , char **){
+    std::cout << "Hello World!!" << std::endl;
+    std::cout <<test::getVersion() << std::endl;
+    
+    return 0;
+} 
+
+
+
+
+
